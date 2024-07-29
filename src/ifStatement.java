579c0070b9fecc6715273675c674238d3517265c
KalpeshJain18 <jainkalpesh597@gmail.com>
2024-07-29 22:07:10 +0530

    second commit

---
+++ src/ifStatement.java
@@ -0,0 +1,20 @@
+
+public class ifStatement {
+
+	public static void main(String[] args) {
+
+		int age = 23;
+
+		if (age == 75) {
+			System.out.println("ok boomer!");
+		} else if (age >= 18) {
+			System.out.println("You are an adult");
+		} else if (age >= 13) {
+			System.out.println("You are an Teenager");
+		} else {
+			System.out.println("You are not adult");
+		}
+
+	}
+
+}
---
+++ src/switchesStatement.java
@@ -0,0 +1,40 @@
+
+public class switchesStatement {
+
+	public static void main(String[] args) {
+
+		String day = "book";
+		
+		switch (day) {
+		
+		case "sunday" : System.out.println("It is sunday!");
+		break;
+		
+		case "monday" : System.out.println("It is monday!");
+		break;
+		
+		case "tuesday" : System.out.println("It is tuesday!");
+		break;
+		
+		case "wednesday" : System.out.println("It is wednesday!");
+		break;
+		
+		case "thursday" : System.out.println("It is thursday!");
+		break;
+		
+		case "friday" : System.out.println("It is friday!");
+		break;
+		
+		case "saturday" : System.out.println("It is saturday!");
+		break;
+		
+		default :System.out.println("It is not an day");
+		}
+				
+		
+		
+		
+		
+	}
+
+}
